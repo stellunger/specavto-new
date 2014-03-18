e69b61497b91f722517c3334b742b20f6e1b3d9b
4X <4X@stellunger.ru>
2014-03-18 19:57:29 +0600

    cats

--- html/application/views/agp.php
+++ html/application/views/agp.php
@@ -8,9 +8,9 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/gaz-33098-19-22-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">ГАЗ 33098<br>Socage 19-22 м</a></div>
 								<ul class="b-details">
 									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
@@ -22,27 +22,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/hyundai-hd78-19-22-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">Hyundai HD78<br>Socage 19-22 м</a></div>
 								<ul class="b-details">
 									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
-								</ul>
-								<a href="#" class="b-link1">Подробнее</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-3 col-sm-4">
-						<div class="b-product-category">
-							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
-							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
-								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
-									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Базовое шасси</span> - Hyundai HD78</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -57,11 +43,39 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/gaz-33106-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">ГАЗ 33106<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ 33106.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/gaz-3302-15-20-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 3302<br>Socage 15-20 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ 3302.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/gaz-33098-15-20-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098<br>Socage 15-20 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
 									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
 								</ul>
@@ -71,13 +85,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/isuzu-npr75l-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">Isuzu NPR75L<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Базовое шасси</span> - Isuzu NPR75L.</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -85,13 +99,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/kamaz-4308-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">КамАЗ 4308<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Базовое шасси</span> - КамАЗ 4308.</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -99,13 +113,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category4.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/kamaz-4326-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">КамАЗ 4326<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Базовое шасси</span> - КамАЗ 4326.</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -113,13 +127,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/ural-4320-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">Урал 4320<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
-									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
+									<li><span>Колесная формула</span> - 6х6.</li>
+									<li><span>Базовое шасси</span> - Урал 4320.</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -127,41 +141,13 @@
 					</div>
 					<div class="col-md-3 col-sm-4">
 						<div class="b-product-category">
-							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-img"><img src="assets/images/catpics/hyundai-hd78-15-20-m.png" class="img-responsive" alt=""></div>
 							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<div class="b-link"><a href="#">Hyundai HD78<br>Socage 15-20 м</a></div>
 								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Высота подъема</span> - от 15 до 20 м.</li>
 									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
-								</ul>
-								<a href="#" class="b-link1">Подробнее</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-3 col-sm-4">
-						<div class="b-product-category">
-							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
-							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
-								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
-									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
-								</ul>
-								<a href="#" class="b-link1">Подробнее</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-3 col-sm-4">
-						<div class="b-product-category">
-							<div class="b-img"><img src="images/category4.png" class="img-responsive" alt=""></div>
-							<div class="b-data">
-								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
-								<ul class="b-details">
-									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
-									<li><span>Колесная формула</span> - 4х2.</li>
-									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+									<li><span>Базовое шасси</span> - Hyundai HD78.</li>
 								</ul>
 								<a href="#" class="b-link1">Подробнее</a>
 							</div>
@@ -169,6 +155,152 @@
 					</div>
 				</div>
 			</div>
-			
+			<div class="b-product-list-category">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Автовышки комбинированные телескопические (АГП)</h2>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/gaz-33106-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33106<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ 33106.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/gaz-23107-14-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 23107<br>Socage 14 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - до 14 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ 23107.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/gaz-33098-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/isuzu-npr75l-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">Isuzu NPR75L<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - Isuzu NPR75L.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/kamaz-4308-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">КамАЗ 4308<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - КамАЗ 4308.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/kamaz-4326-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">КамАЗ 4326<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - КамАЗ 4326.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/kamaz-4326-32-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">КамАЗ 4326<br>Socage 32 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - до 32 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - КамАЗ 4326.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/ural-4320-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">Урал 4320<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 6х6.</li>
+									<li><span>Базовое шасси</span> - Урал 4320.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/hyundai-hd78-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">Hyundai HD78<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - Hyundai HD78.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="assets/images/catpics/hyundai-hd120-20-28-m.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">Hyundai HD120<br>Socage 20-28 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 20 до 28 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - Hyundai HD120.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
