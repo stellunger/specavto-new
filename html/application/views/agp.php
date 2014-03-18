2e0a51e49364e89b6a0f3102d0818f89b71d3dd9
stellunger <1@stellunger.ru>
2014-03-18 15:07:42 +0600

    category agp page

---
+++ html/application/views/agp.php
@@ -0,0 +1,174 @@
+	<!-- ****** Content ****** -->
+	<div class="b-main-content">
+		<div class="container">
+			<div class="b-product-list-category">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Автовышки телескопические (АГП), расположение стрелы над кабиной</h2>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="b-product-list-category">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Автовышки телескопические (АГП), расположение стрелы за кабиной</h2>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category4.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category1.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category2.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category3.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-4">
+						<div class="b-product-category">
+							<div class="b-img"><img src="images/category4.png" class="img-responsive" alt=""></div>
+							<div class="b-data">
+								<div class="b-link"><a href="#">ГАЗ 33098 19-22 м</a></div>
+								<ul class="b-details">
+									<li><span>Высота подъема</span> - от 19 до 22 м.</li>
+									<li><span>Колесная формула</span> - 4х2.</li>
+									<li><span>Базовое шасси</span> - ГАЗ-33098.</li>
+								</ul>
+								<a href="#" class="b-link1">Подробнее</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
