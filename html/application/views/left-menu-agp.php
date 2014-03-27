124fd24f7c2df650f6e3201694fa5f275dcf4322
4X <4X@stellunger.ru>
2014-03-27 16:43:21 +0600

    left-menu-agp

--- html/application/views/left-menu-agp.php
+++ html/application/views/left-menu-agp.php
@@ -1,29 +1,57 @@
-							<!--<ul class="b-right-menu">
-								<li class="active">
-									<a href="#">Автовышки</a>
+							<ul class="b-right-menu">
+								<li>
+									<a href="agp">Автовышки</a>
 									<ul>
-										<li><a href="#">Расположение стрелы над кабиной</a></li>
-										<li><a href="#">Расположение стрелы над кабиной</a></li>
-										<li class="active">
-											<a href="#">Комбинированные телескопические</a>
+										<li><a href="agp">Расположение стрелы над кабиной</a></li>
 											<ul>
-												<li><a href="#">- ГАЗ 203107 14 м</a></li>
-												<li><a href="#">- HYUNDAI-120 20-28</a></li>
-												<li class="active"><a href="#">- КАМАЗ 4308 15-20 м</a></li>
-												<li><a href="#">- ГАЗ 203107 14 м</a></li>
-												<li><a href="#">- HYUNDAI-120 20-28</a></li>
-												<li><a href="#">- КАМАЗ 4308 15-20 м</a></li>
-												<li><a href="#">- ГАЗ 203107 14 м</a></li>
-												<li><a href="#">- HYUNDAI-120 20-28</a></li>
-												<li><a href="#">- КАМАЗ 4308 15-20 м</a></li>
-												<li><a href="#">- ГАЗ 203107 14 м</a></li>
-												<li><a href="#">- HYUNDAI-120 20-28</a></li>
+												<li><a href="agp-gaz-33098-19-22-m">- ГАЗ 33098 19-22 м</a></li>
+												<li><a href="agp-hyundai-hd78-19-22-m">- Hyundai HD78 19-22 м</a></li>
+											</ul>
+										<li><a href="agp">Расположение стрелы за кабиной</a></li>
+											<ul>
+												<li><a href="agp-gaz-33106-15-20-m">- ГАЗ 33106 15-20 м</a></li>
+												<li><a href="agp-gaz-3302-15-20-m">- ГАЗ 3302 15-20 м</a></li>
+												<li><a href="agp-gaz-33098-15-20-m">- ГАЗ 33098 15-20 м</a></li>
+												<li><a href="agp-isuzu-npr75l-15-20-m">- Isuzu NPR75L 15-20 м</a></li>
+												<li><a href="agp-kamaz-4308-15-20-m">- КамАЗ 4308 15-20 м</a></li>
+												<li><a href="agp-kamaz-4326-15-20-m">- КамАЗ 4326 15-20 м</a></li>
+												<li><a href="agp-ural-4320-15-20-m">- Урал 4320 15-20 м</a></li>
+												<li><a href="agp-hyundai-hd78-15-20-m">- Hyundai HD78 15-20 м</a></li>
+											</ul>
+										<li><a href="agp">Комбинированные телескопические</a>
+											<ul>
+												<li><a href="agp-gaz-23107-14-m">- ГАЗ 23107 14 м</a></li>
+												<li><a href="agp-gaz-33106-20-28-m">- ГАЗ 33106 20-28 м</a></li>
+												<li><a href="agp-gaz-33098-20-28-m">- ГАЗ 33098 20-28 м</a></li>
+												<li><a href="agp-isuzu-npr75l-20-28-m">- Isuzu NPR75L 20-28 м</a></li>
+												<li><a href="agp-kamaz-4308-20-28-m">- КамАЗ 4308 20-28 м</a></li>
+												<li><a href="agp-kamaz-4326-20-28-m">- КамАЗ 4326 20-28 м</a></li>
+												<li><a href="agp-kamaz-4326-32-m">- КамАЗ 4326 32 м</a></li>
+												<li><a href="agp-ural-4320-20-28-m">- Урал 4320 20-28 м</a></li>
+												<li><a href="agp-hyundai-hd78-20-28-m">- Hyundai HD78 20-28 м</a></li>
+												<li><a href="agp-hyundai-hd120-20-28-m">- Hyundai HD120 20-28 м</a></li>
 											</ul>
 										</li>
 									</ul>
 								</li>
 								<li><a href="#">Краны-манипуляторы</a></li>
+									<ul>
+												<li><a href="kmu-gaz-33088-450-900-kg">- ГАЗ 33088 450-900 кг</a></li>
+												<li><a href="kmu-gaz-33106-450-900-kg">- ГАЗ 33106 450-900 кг</a></li>
+												<li><a href="kmu-gaz-33098-450-900-kg">- ГАЗ 33098 450-900 кг</a></li>
+												<li><a href="kmu-kamaz-43118-970-1400-kg">- КамАЗ 43118 970-1400 кг</a></li>
+												<li><a href="kmu-isuzu-nlr85a-330-kg">- Isuzu NLR85A 330 кг</a></li>
+												<li><a href="kmu-isuzu-npr75l-450-900-kg">- Isuzu NPR75L 450-900 кг</a></li>
+												<li><a href="kmu-isuzu-cyz51q-1830-kg">- Isuzu CYZ51Q 1830 кг</a></li>
+												<li><a href="kmu-hyundai-hd78-450-620-kg">- Hyundai HD78 450-620 кг</a></li>
+												<li><a href="kmu-hyundai-hd120-520-800-kg">- Hyundai HD120 520-800 кг</a></li>
+												<li><a href="kmu-hyundai-hd170-780-1180-kg">- Hyundai HD170 780-1180 кг</a></li>
+											</ul>
 								<li><a href="#">Эвакуаторы</a></li>
-								<li><a href="#">Автобетонасосы</a></li>
-								<li><a href="#">Электростанции</a></li>
-							</ul>-->
+												<ul>
+												<li><a href="aesp-hyundai-hd78-3520-kg">- Hyundai HD78 до 3250 кг</a></li>
+												<li><a href="aesp-isuzu-npr75l-3540-kg">- Isuzu NPR75L до 3540 кг</a></li>
+												<li><a href="aesp-gaz-33106-3000-kg">- ГАЗ 33106 до 3000 кг</a></li>
+												<li><a href="aesp-kamaz-4308-4950-kg">- КамАЗ 4308 до 4950 кг</a></li>
+											</ul>
+							</ul>
