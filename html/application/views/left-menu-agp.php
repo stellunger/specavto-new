64f25a5066702e244b9864380c2994f805670aec
stellunger <1@stellunger.ru>
2014-03-27 17:01:39 +0600

    left menu bug fixed

--- html/application/views/left-menu-agp.php
+++ html/application/views/left-menu-agp.php
@@ -1,13 +1,14 @@
 							<ul class="b-right-menu">
-								<li>
+								<li class="active">
 									<a href="agp">Автовышки</a>
 									<ul>
-										<li><a href="agp">Расположение стрелы над кабиной</a></li>
+										<li class="active"><a href="#">Расположение стрелы над кабиной</a>
 											<ul>
 												<li><a href="agp-gaz-33098-19-22-m">- ГАЗ 33098 19-22 м</a></li>
 												<li><a href="agp-hyundai-hd78-19-22-m">- Hyundai HD78 19-22 м</a></li>
 											</ul>
-										<li><a href="agp">Расположение стрелы за кабиной</a></li>
+										</li>
+										<li class="active"><a href="#">Расположение стрелы за кабиной</a>
 											<ul>
 												<li><a href="agp-gaz-33106-15-20-m">- ГАЗ 33106 15-20 м</a></li>
 												<li><a href="agp-gaz-3302-15-20-m">- ГАЗ 3302 15-20 м</a></li>
@@ -18,7 +19,8 @@
 												<li><a href="agp-ural-4320-15-20-m">- Урал 4320 15-20 м</a></li>
 												<li><a href="agp-hyundai-hd78-15-20-m">- Hyundai HD78 15-20 м</a></li>
 											</ul>
-										<li><a href="agp">Комбинированные телескопические</a>
+										</li>
+										<li class="active"><a href="agp">Комбинированные телескопические</a>
 											<ul>
 												<li><a href="agp-gaz-23107-14-m">- ГАЗ 23107 14 м</a></li>
 												<li><a href="agp-gaz-33106-20-28-m">- ГАЗ 33106 20-28 м</a></li>
@@ -34,24 +36,26 @@
 										</li>
 									</ul>
 								</li>
-								<li><a href="#">Краны-манипуляторы</a></li>
+								<li><a href="#">Краны-манипуляторы</a>
 									<ul>
-												<li><a href="kmu-gaz-33088-450-900-kg">- ГАЗ 33088 450-900 кг</a></li>
-												<li><a href="kmu-gaz-33106-450-900-kg">- ГАЗ 33106 450-900 кг</a></li>
-												<li><a href="kmu-gaz-33098-450-900-kg">- ГАЗ 33098 450-900 кг</a></li>
-												<li><a href="kmu-kamaz-43118-970-1400-kg">- КамАЗ 43118 970-1400 кг</a></li>
-												<li><a href="kmu-isuzu-nlr85a-330-kg">- Isuzu NLR85A 330 кг</a></li>
-												<li><a href="kmu-isuzu-npr75l-450-900-kg">- Isuzu NPR75L 450-900 кг</a></li>
-												<li><a href="kmu-isuzu-cyz51q-1830-kg">- Isuzu CYZ51Q 1830 кг</a></li>
-												<li><a href="kmu-hyundai-hd78-450-620-kg">- Hyundai HD78 450-620 кг</a></li>
-												<li><a href="kmu-hyundai-hd120-520-800-kg">- Hyundai HD120 520-800 кг</a></li>
-												<li><a href="kmu-hyundai-hd170-780-1180-kg">- Hyundai HD170 780-1180 кг</a></li>
-											</ul>
-								<li><a href="#">Эвакуаторы</a></li>
-												<ul>
-												<li><a href="aesp-hyundai-hd78-3520-kg">- Hyundai HD78 до 3250 кг</a></li>
-												<li><a href="aesp-isuzu-npr75l-3540-kg">- Isuzu NPR75L до 3540 кг</a></li>
-												<li><a href="aesp-gaz-33106-3000-kg">- ГАЗ 33106 до 3000 кг</a></li>
-												<li><a href="aesp-kamaz-4308-4950-kg">- КамАЗ 4308 до 4950 кг</a></li>
-											</ul>
+										<li><a href="kmu-gaz-33088-450-900-kg">- ГАЗ 33088 450-900 кг</a></li>
+										<li><a href="kmu-gaz-33106-450-900-kg">- ГАЗ 33106 450-900 кг</a></li>
+										<li><a href="kmu-gaz-33098-450-900-kg">- ГАЗ 33098 450-900 кг</a></li>
+										<li><a href="kmu-kamaz-43118-970-1400-kg">- КамАЗ 43118 970-1400 кг</a></li>
+										<li><a href="kmu-isuzu-nlr85a-330-kg">- Isuzu NLR85A 330 кг</a></li>
+										<li><a href="kmu-isuzu-npr75l-450-900-kg">- Isuzu NPR75L 450-900 кг</a></li>
+										<li><a href="kmu-isuzu-cyz51q-1830-kg">- Isuzu CYZ51Q 1830 кг</a></li>
+										<li><a href="kmu-hyundai-hd78-450-620-kg">- Hyundai HD78 450-620 кг</a></li>
+										<li><a href="kmu-hyundai-hd120-520-800-kg">- Hyundai HD120 520-800 кг</a></li>
+										<li><a href="kmu-hyundai-hd170-780-1180-kg">- Hyundai HD170 780-1180 кг</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Эвакуаторы</a>
+									<ul>
+										<li><a href="aesp-hyundai-hd78-3520-kg">- Hyundai HD78 до 3250 кг</a></li>
+										<li><a href="aesp-isuzu-npr75l-3540-kg">- Isuzu NPR75L до 3540 кг</a></li>
+										<li><a href="aesp-gaz-33106-3000-kg">- ГАЗ 33106 до 3000 кг</a></li>
+										<li><a href="aesp-kamaz-4308-4950-kg">- КамАЗ 4308 до 4950 кг</a></li>
+									</ul>
+								</li>
 							</ul>
